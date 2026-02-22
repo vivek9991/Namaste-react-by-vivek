da88fdeb3ec8cd67ee733ac905057351add4a6b7
vivechau <vivek.c.chaudhary@oracle.com>
2026-02-22 20:12:01 +0530

    EP2

--- scripts.js
+++ scripts.js
@@ -7,30 +7,12 @@
 </div>; */
 }
 
-const h1 = React.createElement("h1", {}, "Hellow world");
-const h2 = React.createElement("h2", {}, "Vivek");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const h1 = React.createElement("h1", { key: "h1" }, "Hellow world");
+const h2 = React.createElement("h2", { key: "h2" }, "Vivek");
 const childDiv = React.createElement("div", { id: "child" }, [h1, h2]);
 const parentDiv = React.createElement("div", { id: "parent" }, childDiv);
 const root = ReactDOM.createRoot(document.getElementById("rootDiv"));
 root.render(parentDiv);
-
-async function fn() {
-  const response = await fetch("www.google.com");
-  const data = await response.json();
-  console.log(data);
-}
-
-let debouncedFn = throttle(fn, 500);
-
-function throttle(fn, time) {
-  let call = true;
-  return () => {
-    if (call) {
-      call = false;
-      setTimeout(() => {
-        call = true;
-        fn();
-      }, time);
-    }
-  };
-}
