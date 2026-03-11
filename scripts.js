43f10b34394ad49750e4c95d739ea97f170b3aee
vivechau <vivek.c.chaudhary@oracle.com>
2026-03-11 22:38:58 +0530

    EP-3, video-4, JSX and functional component

--- scripts.js
+++ scripts.js
@@ -1,18 +1,16 @@
-{
-  /* <div id="parent">
-  <div id="child">
-    <h1>Hellow world</h1>
-    <h2> Vivek </h2>
-  </div>
-</div>; */
-}
-
-import React from "react";
 import ReactDOM from "react-dom/client";
-
-const h1 = React.createElement("h1", { key: "h1" }, "Hellow world");
-const h2 = React.createElement("h2", { key: "h2" }, "Vivek");
-const childDiv = React.createElement("div", { id: "child" }, [h1, h2]);
-const parentDiv = React.createElement("div", { id: "parent" }, childDiv);
 const root = ReactDOM.createRoot(document.getElementById("rootDiv"));
-root.render(parentDiv);
+const vari = 10;
+const Element = <h1>Element{vari}</h1>;
+const Heading = function () {
+  return (
+    <div>
+      Hello
+      {Element}
+      {setTimeout(() => {
+        alert("hi");
+      }, 5000)}
+    </div>
+  );
+};
+root.render(Heading());
